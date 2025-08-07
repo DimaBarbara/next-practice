207bd7e709b05190018100f7f461c189122bf600
Dmytro <dmytrobarbara1@gmail.com>
2025-08-07 13:52:06 +0300

    add companies page

---
+++ src/app/companies/[id]/page.tsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import Header from '../../components/header';
+
+export interface PageProps {
+  params: { id: string };
+}
+
+export function generateStaticParams() {
+    return []
+}
+
+export default function Page({ params }: PageProps) {
+  return (
+    <>
+      <Header>Company ({params.id})</Header>
+    </>
+  );
+}
