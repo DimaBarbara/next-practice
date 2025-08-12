8304869395c2e018a188cbeedd6bb4ac465411bd
Dmytro <dmytrobarbara1@gmail.com>
2025-08-12 09:30:17 +0300

    add data fetch

--- src/app/components/add-company-button.tsx
+++ src/app/components/add-company-button.tsx
@@ -1,20 +1,14 @@
 'use client'
 
-import React, { useState } from 'react'
+import React from 'react';
+import { useRouter } from 'next/navigation';
 import Button from './button'
-import CompanyFormModal from './company-form-modal'
+
 
 export default function AddCompanyButton() {
-    const[show, setShow] = useState(false)
+    const router = useRouter();
   return (
-    <>
-    <Button onClick={()=> setShow(true)}>Add company</Button>
-    <CompanyFormModal
-    onSubmit={console.log}
-    show={show}
-    onClose={() => setShow(false)}
-    />
-    </>
+    <Button onClick={() => router.push('/companies/new')}>Add company</Button>
   )
 }
 
